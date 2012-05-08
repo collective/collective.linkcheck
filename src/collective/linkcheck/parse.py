83d5fbab9285e4d553631916dde845ead05008a5
Malthe Borch <mborch@gmail.com>
2012-05-08 22:39:42 +0200

    Log parse warnings, catch and ignore.

--- src/collective/linkcheck/parse.py
+++ src/collective/linkcheck/parse.py
@@ -1,8 +1,18 @@
+import logging
+
 import lxml.html
 import lxml.etree
 
+logger = logging.getLogger("linkcheck.parse")
+
+
 def iter_links(body):
-    html = lxml.html.fromstring(body)
+    try:
+        html = lxml.html.fromstring(body)
+    except lxml.etree.ParserError as exc:
+        logger.warn(exc)
+        return
+
     tree = html.getroottree()
 
     for link in html.iterfind('.//a'):
