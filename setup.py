046340f1a192291872da25222fb890f28b1458ec
Philip Bauer <bauer@starzel.de>
2017-10-10 14:47:24 +0200

    add buildout from a fresh bobtemplates.plone template

--- setup.py
+++ setup.py
@@ -1,56 +1,67 @@
-import os
-import sys
+# -*- coding: utf-8 -*-
+"""Installer for the collective.linkcheck package."""
 
-reload(sys).setdefaultencoding("UTF-8")
+from setuptools import find_packages
+from setuptools import setup
 
-from setuptools import setup, find_packages
+
+long_description = '\n\n'.join([
+    open('README.rst').read(),
+    open('CONTRIBUTORS.rst').read(),
+    open('CHANGES.rst').read(),
+])
 
 
-def read(*pathnames):
-    return open(os.path.join(os.path.dirname(__file__), *pathnames)).read().\
-           decode('utf-8')
-
-version = '1.5.dev0'
-
-setup(name='collective.linkcheck',
-      version=version,
-      description="Add-on for Plone that provides link "
-                  "validity checking and reporting.",
-      long_description='\n'.join([
-          read('README.rst'),
-          read('CHANGES.rst'),
-          ]),
-      classifiers=[
+setup(
+    name='collective.linkcheck',
+    version='1.5.dev0',
+    description="Add-on for Plone that provides link validity checking and reporting.",
+    long_description=long_description,
+    # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Environment :: Web Environment",
         "Framework :: Plone",
-        "Framework :: Zope2",
-        "Development Status :: 5 - Production/Stable",
+        "Framework :: Plone :: 5.0",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+    ],
+    keywords='Python Plone',
+    author='Malthe Borch',
+    author_email='mborch@gmail.com',
+    url='https://pypi.python.org/pypi/collective.linkcheck',
+    license='GPL version 2',
+    packages=find_packages('src', exclude=['ez_setup']),
+    namespace_packages=['collective'],
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        # -*- Extra requirements: -*-
+        'setuptools',
+        'zc.queue >= 1.3',
+        'requests',
+        'plone.z3cform',
+        'plone.api',
+        'tablib',
+    ],
+    extras_require={
+        'test': [
+            'plone.app.testing',
+            # Plone KGS does not use this version, because it would break
+            # Remove if your package shall be part of coredev.
+            # plone_coredev tests as of 2016-04-01.
+            'plone.testing>=5.0.0',
+            'plone.app.contenttypes',
+            'plone.app.robotframework[debug]',
         ],
-      keywords='plone link-checking',
-      author='Malthe Borch',
-      author_email='mborch@gmail.com',
-      license="GPLv2+",
-      packages=find_packages('src'),
-      package_dir={'': 'src'},
-      namespace_packages=['collective'],
-      include_package_data=True,
-      zip_safe=False,
+    },
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
 
-      # If the dependency to z3c.form gives you trouble within a Zope
-      # 2 environment, try the `fakezope2eggs` recipe
-      install_requires=[
-          'setuptools',
-          'zc.queue >= 1.3',
-          'requests',
-          'plone.z3cform',
-          'plone.api',
-          'tablib',
-      ],
-      entry_points="""
-      [z3c.autoinclude.plugin]
-      target = plone
-
-      [zopectl.command]
-      linkcheck = collective.linkcheck:processor
-      """,
-      )
+    [zopectl.command]
+    linkcheck = collective.linkcheck:processor
+    """,
+)
